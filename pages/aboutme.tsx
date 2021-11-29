9e46de881d59835489a075382a7a934f24ddb5d3
Will <tayloe93@gmail.com>
2021-11-28 20:15:49 -0600

    initial commit

---
+++ pages/aboutme.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import NavBar from '../components/Navbar'
+import styles from '../styles/Home.module.css'
+
+const AboutMe: NextPage = () => {
+    return(
+        <>
+        <NavBar />
+        <div className={styles.container}>
+            <div>About Me</div>
+        </div>
+        </>
+    )
+}
+
+export default AboutMe
