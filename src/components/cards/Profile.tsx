002e40c51b5523083154ce428438c2410f70f82a
Will <tayloe93@gmail.com>
2022-01-24 22:05:32 -0600

    added profile data from github

--- src/components/cards/Profile.tsx
+++ src/components/cards/Profile.tsx
@@ -5,11 +5,15 @@ interface componentProps {
     about: string;
 }
 
-function profileCard(props : componentProps) {
+const ProfileCard = (props : componentProps) => {
     return (
-        <div className="container columns-2">
-            <img src={props.image} alt="profile image"></img>
-            <div className="text-left">{props.about}</div>
+        <div className="container h-full space-y-4">
+            <div className='mx-auto w-1/2'>
+                <img src={props.image} alt="profile image" className="rounded-full"/>
+            </div>
+            <div className="mx-auto w-1/2 text-left">{props.about}</div>
         </div>
     )
 }
+
+export default ProfileCard;
