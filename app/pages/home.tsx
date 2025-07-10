95ac8866af347874d23db468d5f44bd23eea93fa
Will Shivers <tayloe93@gmail.com>
2025-07-10 00:42:03 -0500

    refactor: Remove default react router content

---
+++ app/pages/home.tsx
@@ -0,0 +1,17 @@
+export function Home() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+        <header className="flex flex-col items-center gap-9">
+          <div className="w-[500px] max-w-[100vw] p-4">
+            HOME
+          </div>
+        </header>
+        <div className="max-w-[300px] w-full space-y-6 px-4">
+          TEST
+        </div>
+      </div>
+    </main>
+  );
+}
+
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Home } from "../pages/home";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
-export default function Home() {
-  return <Welcome />;
+export default function HomeRoute() {
+  return <Home />;
 }
